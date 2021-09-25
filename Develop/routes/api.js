09631cbeabf38f89ee2c26265aa5fe6336fec908
Josh Nolan <nolanjosh1979@gmail.com>
2021-09-25 16:15:04 -0500

    deploying

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,35 +1,16 @@
 const router = require("express").Router();
-const Transaction = require("../models/Exercise.js");
+const Exercise = require("../models/Exercise");
 
-router.post("/api/transaction", ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.get("/api/workouts", (req, res)=> {
+    Exercise.aggregate([
+        { $addFields: {totalDuration: {$sum: "$exercises.duration"}}}
+    ])
+        .then (workouts => {
+            res.json(workouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
 module.exports = router;
