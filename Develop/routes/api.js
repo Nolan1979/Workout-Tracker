eede3d892d9a048887385b8eed9547fca741e26f
Josh Nolan <nolanjosh1979@gmail.com>
2021-09-25 16:41:06 -0500

    added routes

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -13,4 +13,37 @@ router.get("/api/workouts", (req, res)=> {
         });
 });
 
+router.post("/api/workouts", (req, res) => {
+  Exercise.create(req.body)
+  .then(workouts => {
+      res.json(workouts);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  });
+});
+
+router.get("/api/workouts/range", (req, res)=> {
+  Exercise.aggregate([
+      { $addFields: {totalDuration: {$sum: "$exercises.duration"}}}
+  ])
+      .then (workouts => {
+          res.json(workouts);
+      })
+      .catch(err => {
+          res.status(400).json(err);
+      });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Exercise.findByIdAndUpdate(
+    { _id: req.params.id }, { $push: {exercises: req.body} }
+  ).then((workout) => {
+    res.json(workout);
+  }).catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+
 module.exports = router;
