ee4918846e0816ac857873d9b987a33dc3981c4e
Josh Nolan <nolanjosh1979@gmail.com>
2021-10-02 13:53:16 -0500

    added api routes and README

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
+const path = require('path');
 const Exercise = require("../models/Exercise");
 
 router.get("/api/workouts", (req, res)=> {
     Exercise.aggregate([
         { $addFields: {totalDuration: {$sum: "$exercises.duration"}}}
-    ])
+    ])  .sort({_id: -1})
+        .limit(7)
         .then (workouts => {
             res.json(workouts);
         })
@@ -43,6 +45,14 @@ router.put("/api/workouts/:id", (req, res) => {
   }).catch(err => {
     res.status(400).json(err);
   });
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 
---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Workout-Tracker
+
+## Description
+This is a Workout app that allows users to create new workouts and track their progress!
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+Clone the repo and run `npm start` in your terminal. Navigate to `localhost:3000` in your browser!
+
+## Usage
+Create a new exercise to track. Fill in the input fields. Track your progress!
+
+## License
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+
+## Questions
+Having an issue? Email me with any questions about the app nolanjosh1979@gmail.com
